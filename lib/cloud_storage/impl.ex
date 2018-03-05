b97893a204cc89ea1de034562210ddb3c6e99bcf
Rickard Andersson <gonz@severnatazvezda.com>
2018-03-05 17:36:19 +0200

    fix this mess
    Previously everything was split out into this stupidity with `use`ing
    in order to just insert code, which is a useless usage of macros here.

--- lib/cloud_storage/impl.ex
+++
@@ -1,9 +0,0 @@
-defmodule GCloudex.CloudStorage.Impl do
-  @moduledoc """
-  Wrapper for Google Cloud Storage API.
-  """
-  defmacro __using__(:cloud_storage) do
-    quote location: :keep do
-    end
-  end
-end
